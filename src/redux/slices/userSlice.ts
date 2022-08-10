8f2899a7d1a505921d2b83c04141b52b9fe4c6a9
Nihal Walia <kartheekenumarthi@gmail.com>
2022-08-10 13:25:56 +0530

    fix: remove unwanted reducers

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
-import { LoginUserPayload, User } from '../../types/user';
+import { User } from '../../types/user';
 
 export interface UserState {
   token: string;
@@ -18,21 +17,8 @@ const initialState: UserState = {
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {
-    loginUser: (state, action: PayloadAction<LoginUserPayload>) => {
-      state.token = action.payload.token;
-      state.isAuthenticated = true;
-      state.isInitialized = true;
-      state.user = action.payload.user;
-    },
-    logoutUser: (state) => {
-      state.token = '';
-      state.isAuthenticated = false;
-      state.user = undefined;
-    },
-  },
+  reducers: {},
 });
 
-export const { loginUser, logoutUser } = userSlice.actions;
-
+export const { actions } = userSlice;
 export default userSlice.reducer;
