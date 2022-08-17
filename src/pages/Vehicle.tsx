bef48d36f6d3dea53afe9ab7dd2e22a6b3da4ab1
Nihal Walia <kartheekenumarthi@gmail.com>
2022-08-17 15:24:24 +0530

    fix: react refresh package issue

--- src/pages/Vehicle.tsx
+++ src/pages/Vehicle.tsx
@@ -1,14 +1,25 @@
-import { Box, Card, Grid } from '@mui/material';
 import React from 'react';
+
+import { Box, Card, Grid } from '@mui/material';
+import Tabs, { TabItem } from '../components/Tabs';
 import Page from '../components/Page/Page';
 
 const Vehicle = () => {
+  const tabItems: TabItem[] = [
+    {
+      label: 'Overview',
+      content: <div>overview</div>,
+    },
+  ];
+
   return (
     <Page sx={{ bgcolor: (theme) => theme.palette.grey[300] }}>
       <Box>Header component</Box>
       <Grid container spacing={3}>
         <Grid item sm={12} md={6} lg={4}>
-          <Card sx={{ p: 2 }} />
+          <Card sx={{ p: 2 }}>
+            <Tabs tabItems={tabItems} orientation="vertical" />
+          </Card>
         </Grid>
         <Grid item xs={12} md={6} lg={8}>
           Vehicle details
