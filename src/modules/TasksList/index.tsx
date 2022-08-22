540ae2d2c45739c0222bc85bb2a2a7044387f40b
Nihal Walia <kartheekenumarthi@gmail.com>
2022-08-22 15:38:33 +0530

    fix: update customer details component

--- src/modules/TasksList/index.tsx
+++ src/modules/TasksList/index.tsx
@@ -7,10 +7,10 @@ import CarList from '../CarList';
 const TasksList = () => {
   return (
     <Grid container sx={{ mt: 2 }} spacing={4}>
-      <Grid item sm={12} md={8}>
+      <Grid item sm={12} md={7}>
         <CarList />
       </Grid>
-      <Grid item sm={12} md={4}>
+      <Grid item sm={12} md={5}>
         <CustomerDetailsSection />
       </Grid>
     </Grid>
