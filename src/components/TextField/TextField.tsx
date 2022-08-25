53ddc61a7c7f2bca28faf8f5a578ff696c62395d
karthik enumarthi <kartheekenumarthi@gmail.com>
2022-08-25 11:59:21 +0530

    enhance: ag-grid vehicles list (#33)
    * customize ag grid
    
    * update filters
    
    * add text field story
    
    * fix: update url

---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,41 @@
+import { InputBase, InputBaseProps, Stack, styled } from '@mui/material';
+import React, { FC, ReactNode } from 'react';
+
+type Props = {
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+} & InputBaseProps;
+
+const Input = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 2),
+    width: '100%',
+  },
+}));
+
+const InputWrapper = styled(Stack)(({ theme }) => ({
+  backgroundColor: theme.palette.grey[200],
+  borderRadius: theme.shape.borderRadius,
+}));
+
+const TextField: FC<Props> = ({ startIcon, endIcon, ...other }) => {
+  return (
+    <InputWrapper direction="row" alignItems="center" justifyContent="center">
+      {startIcon && (
+        <Stack alignItems="center" justifyContent="center" p={1}>
+          {startIcon}
+        </Stack>
+      )}
+      <Input {...other} />
+      {endIcon && (
+        <Stack alignItems="center" justifyContent="center" p={1}>
+          {endIcon}
+        </Stack>
+      )}
+    </InputWrapper>
+  );
+};
+
+export default TextField;
--- src/modules/VehiclesGrid/index.tsx
+++ src/modules/VehiclesGrid/index.tsx
@@ -1,8 +1,10 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import { Box } from '@mui/material';
 import moment from 'moment';
 
+import { IVehiclesData } from '../../types/vehicle';
+import { PATHS } from '../../utils/constants';
 import Link from '../../components/Link/Link';
 import Chip from '../../components/Chip/Chip';
 import Progress from '../../components/Progress/Progress';
@@ -10,16 +12,18 @@ import vehicles from './vehicles.mock';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import './custom.css';
 import 'ag-grid-enterprise';
+import GridTopActions from './GridTopActions';
 
-const customerCell = ({ value }: any) => <Link to="/"> {value} </Link>;
+const customerCell = ({ value }: any) => <Link to={PATHS.dashboard}> {value} </Link>;
 const customerStatusCell = ({ value }: any) =>
   value && (
     <Box py={1}>
       <Chip text={value} />
     </Box>
   );
-const VINCell = ({ value }: any) => <Link to="/"> {value} </Link>;
+const VINCell = ({ value }: any) => <Link to={PATHS.dashboard}> {value} </Link>;
 const stageCell = ({ value }: any) =>
   value && (
     <Box py={1}>
@@ -30,63 +34,105 @@ const statusEntryDateCell = ({ value }: any) => value && moment(value).format('M
 const lastContactCell = ({ value }: any) => value && moment(value).format('MMM DD, YYYY');
 const deliveryReadinessCell = ({ value }: any) => <Progress progress={value} />;
 
-const VehiclesGrid = () => {
-  const columnDefs = [
-    { field: 'customer', cellRenderer: customerCell },
-    { field: 'customerStage', filter: 'agSetColumnFilter' },
-    { field: 'customerStatus', cellRenderer: customerStatusCell, width: 170, filter: 'agSetColumnFilter' },
-    { field: 'VIN', cellRenderer: VINCell, width: 160 },
-    { field: 'model', width: 120, filter: 'agSetColumnFilter' },
-    { field: 'color', width: 160, filter: 'agSetColumnFilter' },
-    { field: 'stage', cellRenderer: stageCell, width: 100 },
-    { field: 'status', width: 120, filter: 'agSetColumnFilter' },
-    {
-      field: 'statusEntryDate',
-      cellRenderer: statusEntryDateCell,
-      width: 180,
-      filter: 'agDateColumnFilter',
-    },
-    { field: 'duration', headerName: 'Duration(days)', width: 160, filter: 'agNumberColumnFilter' },
-    {
-      field: 'deliveryReadiness',
-      headerName: 'Delivery Readiness',
-      cellRenderer: deliveryReadinessCell,
-      filter: 'agNumberColumnFilter',
-    },
-    { field: 'software', width: 120, filter: 'agNumberColumnFilter' },
-    {
-      field: 'lastContact',
-      cellRenderer: lastContactCell,
-      width: 140,
-      filter: 'agDateColumnFilter',
-    },
-    { field: 'ownership', width: 140 },
-  ];
+const columnDefs = [
+  { field: 'customer', cellRenderer: customerCell, width: 130, filter: 'agTextColumnFilter' },
+  { field: 'customerStage' },
+  { field: 'customerStatus', cellRenderer: customerStatusCell, width: 170 },
+  { field: 'VIN', cellRenderer: VINCell, width: 160, filter: 'agTextColumnFilter' },
+  { field: 'model', width: 120 },
+  { field: 'color', width: 160 },
+  { field: 'stage', cellRenderer: stageCell, width: 100 },
+  { field: 'status', width: 120 },
+  {
+    field: 'statusEntryDate',
+    cellRenderer: statusEntryDateCell,
+    width: 160,
+    filter: 'agDateColumnFilter',
+  },
+  { field: 'duration', headerName: 'Duration(days)', width: 160, filter: 'agNumberColumnFilter' },
+  {
+    field: 'deliveryReadiness',
+    headerName: 'Delivery Readiness',
+    cellRenderer: deliveryReadinessCell,
+    filter: 'agNumberColumnFilter',
+  },
+  { field: 'software', width: 120, filter: 'agNumberColumnFilter' },
+  {
+    field: 'lastContact',
+    cellRenderer: lastContactCell,
+    width: 140,
+    filter: 'agDateColumnFilter',
+  },
+  { field: 'ownership', width: 140 },
+];
 
-  const defaultColDef = useMemo(
-    () => ({
-      sortable: true,
-      filter: true,
-      enableRowGroup: true,
-      resizable: true,
-      autoSize: true,
-    }),
-    []
-  );
+const defaultColDef = {
+  sortable: true,
+  filter: true,
+  enableRowGroup: true,
+  resizable: true,
+  autoSize: true,
+};
+
+const rowData: IVehiclesData[] = vehicles;
+
+const gridOptions = {
+  suppressMenuHide: false,
+  defaultColDef,
+  enableCharts: true,
+  animateRows: true,
+  enableRangeSelection: true,
+  rowDragManaged: true,
+  headerHeight: 30,
+  rowHeight: 30,
+  pivotPanelShow: 'always',
+  pivotColumnGroupTotals: 'before',
+  pivotRowTotals: 'before',
+  sideBar: {
+    toolPanels: [
+      {
+        id: 'columns',
+        labelDefault: 'Columns',
+        labelKey: 'columns',
+        iconKey: 'columns',
+        toolPanel: 'agColumnsToolPanel',
+      },
+      {
+        id: 'filters',
+        labelDefault: 'Filters',
+        labelKey: 'filters',
+        iconKey: 'filter',
+        toolPanel: 'agFiltersToolPanel',
+      },
+    ],
+  },
+  columnDefs,
+  rowData,
+  pagination: true,
+};
+
+const VehiclesGrid = () => {
+  const gridRef = useRef<AgGridReact<IVehiclesData>>(null);
+
+  const onSearch = useCallback((searchText: string) => {
+    gridRef.current?.api.setQuickFilter(searchText);
+  }, []);
+
+  const onToggleToolPanelClick = useCallback((panelId: 'columns' | 'filters') => {
+    if (gridRef.current?.api.getOpenedToolPanel() === panelId) {
+      gridRef.current?.api.closeToolPanel();
+    } else {
+      gridRef.current?.api.openToolPanel(panelId);
+    }
+  }, []);
 
   return (
-    <Box width="100%" height="calc(100vh - 120px)" className="ag-theme-material">
-      <AgGridReact
-        rowGroupPanelShow="always"
-        rowData={vehicles}
-        columnDefs={columnDefs}
-        defaultColDef={defaultColDef}
-        rowSelection="multiple"
-        animateRows
-        pagination
-        sideBar={['filters', 'columns']}
-      />
-    </Box>
+    <>
+      <GridTopActions onSearch={onSearch} onToggleToolPanelClick={onToggleToolPanelClick} />
+      <Box width="100%" height="calc(100vh - 150px)" className="ag-theme-material">
+        <AgGridReact {...gridOptions} ref={gridRef} />
+      </Box>
+    </>
   );
 };
 
