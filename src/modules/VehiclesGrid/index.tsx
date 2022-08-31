d7d5c943444687adac91a0dffafdf67706d68077
AravinthSelvarajCopancs <110714826+AravinthSelvarajCopancs@users.noreply.github.com>
2022-08-31 15:25:10 +0530

    ag-grid-filters-enhancement (#39)

--- src/modules/VehiclesGrid/index.tsx
+++ src/modules/VehiclesGrid/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import { Box } from '@mui/material';
 import moment from 'moment';
+import { FilterChangedEvent } from 'ag-grid-community';
 
 import { IVehiclesData } from '../../types/vehicle';
 import { PATHS } from '../../utils/constants';
@@ -15,6 +16,7 @@ import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import './custom.css';
 import 'ag-grid-enterprise';
 import GridTopActions from './GridTopActions';
+import AppliedFilters from './AppliedFilters';
 
 const customerCell = ({ value }: any) => <Link to={PATHS.dashboard}> {value} </Link>;
 const customerStatusCell = ({ value }: any) =>
@@ -36,7 +38,7 @@ const deliveryReadinessCell = ({ value }: any) => <Progress progress={value} />;
 
 const columnDefs = [
   { field: 'customer', cellRenderer: customerCell, width: 130, filter: 'agTextColumnFilter' },
-  { field: 'customerStage' },
+  { field: 'customerStage' }, // , filterParams: { buttons: ['apply', 'clear'] } },
   { field: 'customerStatus', cellRenderer: customerStatusCell, width: 170 },
   { field: 'VIN', cellRenderer: VINCell, width: 160, filter: 'agTextColumnFilter' },
   { field: 'model', width: 120 },
@@ -112,6 +114,7 @@ const gridOptions = {
 };
 
 const VehiclesGrid = () => {
+  const [filterModel, setFilterModel] = useState<{ [key: string]: any }>();
   const gridRef = useRef<AgGridReact<IVehiclesData>>(null);
 
   const onSearch = useCallback((searchText: string) => {
@@ -126,11 +129,18 @@ const VehiclesGrid = () => {
     }
   }, []);
 
+  const onFilterChanged = (event: FilterChangedEvent<IVehiclesData>) => {
+    setFilterModel(event.api.getFilterModel());
+  };
+
   return (
     <>
       <GridTopActions onSearch={onSearch} onToggleToolPanelClick={onToggleToolPanelClick} />
+      {filterModel && !!Object.keys(filterModel).length && (
+        <AppliedFilters filterModel={filterModel} gridRef={gridRef} />
+      )}
       <Box width="100%" height="calc(100vh - 150px)" className="ag-theme-material">
-        <AgGridReact {...gridOptions} ref={gridRef} />
+        <AgGridReact {...gridOptions} ref={gridRef} onFilterChanged={onFilterChanged} />
       </Box>
     </>
   );
